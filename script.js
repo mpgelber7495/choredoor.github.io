fa99bdb0a30ea2aebad810a67d882a54d2dde4a2
Michael Gelber <mgelber@Michael-Gelber.local>
2019-07-19 14:20:46 -0400

    Adding Toms Face

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
 
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg"
+let botDoorPath = "https://imgur.com/5FWCxaJ"
 
 let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
 
